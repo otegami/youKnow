bd51a6649f7a943a42b9b52a0239ea9e4d7203f4
otegami <a.s.takuya1026@gmail.com>
2020-03-07 23:05:45 +0900

    [progressing] creating the modelof users

---
+++ src/spec/models/user_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  describe "Validation" do
+    it "should check the existence" do
+      expect(@user.valid?).to eq (true)
+    end
+    it "should check the existence of name" do
+      @user.name = ""
+      expect(@user.invalid?).to eq (true)
+    end
+    it "should check the existence of email" do
+      @user.email = ""
+      expect(@user.invalid?).to eq (true)
+    end
+    it "should check whether the length of name is over 51 or not." do
+      @user.name = "a" * 51
+      expect(@user.invalid?).to eq (true)
+    end
+    it "should check whether the length of email is over 256 or not." do
+      @user.name = "a" * 244 + "@example.com"
+      expect(@user.invalid?).to eq (true)
+    end
+    it "should accept valid addresses" do
+      valid_addresses = %w[user@example.com USER@foo.COM A__US_ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+      valid_addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user.valid?).to eq (true)
+      end
+    end
+    it "should reject invalid addresses" do
+      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+      invalid_addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user.invalid?).to eq (true)
+      end
+    end
+    it "should check whether the address is unique or not" do
+      duplicate_user = @user.dup
+      duplicate_user.email = @user.email.upcase
+      @user.save!
+      expect(@user.invalid?).to eq (true)
+    end  
+  end
+end
--- src/spec/requests/users_request_spec.rb
+++ src/spec/requests/users_request_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-
-  describe "GET /new" do
+  describe "GET /signup" do
     it "returns http success" do
-      get "/users/new"
+      get signup_path
       expect(response).to have_http_status(:success)
     end
   end
