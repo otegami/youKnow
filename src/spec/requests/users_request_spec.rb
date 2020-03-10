871eb5fc98434aa39f00a203c9e59fbb088d818b
otegami <a.s.takuya1026@gmail.com>
2020-03-10 11:50:44 +0900

    [Finish] to create the function of singup

--- src/spec/requests/users_request_spec.rb
+++ src/spec/requests/users_request_spec.rb
@@ -7,4 +7,36 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "Post /signup" do
+    before do
+      get signup_path
+    end  
+
+    context "when the data is invalid " do
+      it "should return http success" do
+        post users_path, params: { user: FactoryBot.attributes_for(:invalidUser) }
+        expect(response).to have_http_status(:success)
+      end
+
+      it "should not create new user" do
+        expect{
+          post users_path, params: { user: FactoryBot.attributes_for(:invalidUser) }
+        }.to change{ User.count }.by(0)
+      end
+    end
+    
+    context "when the data is valid " do
+      it "should return http success" do
+        post users_path, params: { user: FactoryBot.attributes_for(:user) }
+        expect(response).to have_http_status(302)
+      end
+
+      it "should create new user" do
+        expect{
+          post users_path, params: { user: FactoryBot.attributes_for(:user) }
+        }.to change{ User.count }.by(1)
+      end
+    end  
+  end  
 end
