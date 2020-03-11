300f4ca69b2a35e0797f37cff986f4627435ad7c
otegami <a.s.takuya1026@gmail.com>
2020-03-11 17:05:22 +0900

    Implement basic login

---
+++ src/spec/systems/users_login_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+RSpec.describe "Log in", type: :system do
+  describe "The process of loging in" do
+    context "with using invalid information" do
+      it "should show the errors" do
+        visit login_path
+
+        fill_in "Email", with: ""
+        fill_in "Password", with: ""
+        click_button "Log in"
+
+        expect(page).to have_text("invalid email / password combination")
+
+        visit root_path
+        expect(page).not_to have_text("invalid email / password combination")
+      end  
+    end
+    context "with using valid information" do
+      let!(:user) { FactoryBot.create(:user) }
+      
+      it "should show user's profile page" do
+        visit login_path
+
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+        click_button "Log in"
+
+        expect(page).to have_link 'Log in', href: login_path, count: 0
+        expect(page).to have_link 'Log out', href: logout_path
+        expect(page).to have_link 'Profile', href: user_path(user)
+
+        click_link "Log out"
+
+        expect(page).to have_link 'Log in', href: login_path
+        expect(page).to have_link 'Log out', href: logout_path, count: 0
+        expect(page).to have_link 'Profile', href: user_path(user), count: 0
+
+      end  
+    end  
+  end  
+end 
