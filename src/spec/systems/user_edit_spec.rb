f3c8470d0fde8d78a3ec8098eeb78c9c73974da9
otegami <a.s.takuya1026@gmail.com>
2020-03-13 21:06:05 +0900

    [Start] create updating-users by TDD

---
+++ src/spec/systems/user_edit_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+RSpec.describe "Editting user", type: :system do
+  let!(:user) { FactoryBot.create(:user) }
+
+  context "with the invalid information" do
+    it "should return errors about validationes" do
+      visit edit_user_path(user)
+
+      fill_in "Name", with: ""
+      fill_in "Email", with: ""
+      fill_in "Password", with: ""
+      fill_in "Confirmation", with: ""
+      click_button "Save changes"
+
+      expect(page).to have_text("The form contains 5 errors")
+    end
+  end
+  context "with the valid information" do
+    it "return the profile page" do
+      visit edit_user_path(user)
+
+      fill_in "Name", with: "user name is changed"
+      fill_in "Email", with: "user email is changed"
+      fill_in "Password", with: ""
+      fill_in "Confirmation", with: ""
+      click_button "Save changes"
+
+      expect(page).to have_text("user name is changed")
+    end  
+  end
+end  
