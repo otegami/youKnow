92ffb7026a1dd9238fdc3911c3ae5bfabc44087d
otegami <a.s.takuya1026@gmail.com>
2020-03-15 21:39:42 +0900

    [Progressing] create rspec about security of editting user

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -42,5 +43,13 @@ class UsersController < ApplicationController
         :password,
         :password_confirmation
       )
+    end 
+  
+   # check whether the user has logged in or not
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in"
+        redirect_to login_path
+      end 
     end 
 end
--- src/spec/systems/user_edit_spec.rb
+++ src/spec/systems/user_edit_spec.rb
@@ -1,38 +1,56 @@
 require 'rails_helper'
 RSpec.describe "Editting user", type: :system do
+
   let!(:user) { FactoryBot.create(:user) }
+  let!(:the_other_user) { FactoryBot.create(:user) }
 
-  before do
-    visit login_path
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Log in"
-  end  
+  describe "information by user who loged in " do
+    before do
+      visit login_path
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      click_button "Log in"
+    end  
 
-  context "with the invalid information" do
-    it "should return errors about validationes" do
-      visit edit_user_path(user)
+    context "with the invalid information" do
+      it "should return errors about validationes" do
+        visit edit_user_path(user)
 
-      fill_in "Name", with: ""
-      fill_in "Email", with: ""
-      fill_in "Password", with: ""
-      fill_in "Confirmation", with: ""
-      click_button "Save changes"
+        fill_in "Email", with: ""
+        fill_in "Name", with: ""
+        fill_in "Password", with: ""
+        fill_in "Confirmation", with: ""
+        click_button "Save changes"
 
-      expect(page).to have_text("The form contains 3 errors")
+        expect(page).to have_text("The form contains 3 errors")
+      end
+    end
+    context "with the valid information" do
+      it "return the profile page" do
+        visit edit_user_path(user)
+
+        fill_in "Name", with: "user name is changed"
+        fill_in "Email", with: "emailIsChanged@gmail.com"
+        fill_in "Password", with: ""
+        fill_in "Confirmation", with: ""
+        click_button "Save changes"
+
+        expect(page).to have_text("user name is changed")
+      end  
     end
   end
-  context "with the valid information" do
-    it "return the profile page" do
+
+  describe "information by user who don't log in " do
+    it "should return log in page" do
       visit edit_user_path(user)
-
-      fill_in "Name", with: "user name is changed"
-      fill_in "Email", with: "user email is changed"
-      fill_in "Password", with: ""
-      fill_in "Confirmation", with: ""
-      click_button "Save changes"
-
-      expect(page).to have_text("user name is changed")
-    end  
+      expect(page).to have_text("Please log in")
+    end
   end
+
+  describe "information by the other user" do
+    it "should return log in page" do
+      visit edit_user_path(the_other_user)
+      expect(page).to have_text("Please log in")
+    end
+  end  
 end  
