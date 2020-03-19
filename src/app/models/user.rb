4c4231fe4c2fd8a7ecb4250ed0787b35544782e6
otegami <a.s.takuya1026@gmail.com>
2020-03-19 16:46:35 +0900

    [Start] to create function of user reconfiguring password

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	before_create :create_activation_digest
 	before_save :downcase_email
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -47,6 +47,19 @@ class User < ApplicationRecord
 	def forget
 		update_attribute(:remember_digest, nil)
 	end
+
+	# Set attributes for reconfiguring user password
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest, User.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)
+		# update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+	end	
+
+	# Send the email of configuring user password to user
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
+	end	
 
 	private
 		# Downcase user email 
