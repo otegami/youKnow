47f55f5a3379a75b33ee5bf07c4b66c6aaa82bf2
otegami <a.s.takuya1026@gmail.com>
2020-03-31 18:33:51 +0900

    [Progressing] create spec about project top page

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
 	before_save :downcase_email
 	has_many :projects, dependent: :destroy
 	has_many :members
-	has_many :projects, through: :members
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, length: { maximum: 255 },
