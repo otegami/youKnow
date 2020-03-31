e58f0f23d15003deae517aa4685b1a38765d721d
otegami <a.s.takuya1026@gmail.com>
2020-03-31 16:49:29 +0900

    [Progressing] crate spec and controller about index action

--- src/spec/requests/members_request_spec.rb
+++ src/spec/requests/members_request_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Members", type: :request do
+  let(:project){ FactoryBot.create(:project_with_members) }
 
+  # user is a member of project or not
+  describe "Get /projects/:project_id/members" do
+    before do
+      user = project.owner
+      post login_path, params: { session: {email: user.email, password: user.password}}
+    end
+    
+    it "should show the index page about member" do
+      get  project_members_path(project)
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
