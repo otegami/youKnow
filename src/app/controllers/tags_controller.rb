1aa2fc592655232e693d02e7c180fbe592fabff4
otegami <a.s.takuya1026@gmail.com>
2020-04-15 15:07:24 +0900

    [Finish] create to system spec and controller about tags

--- src/app/controllers/tags_controller.rb
+++ src/app/controllers/tags_controller.rb
@@ -1,6 +1,8 @@
 class TagsController < ApplicationController
   before_action :logged_in_user
   before_action :correct_member, only: :index
+  before_action :manage_tags, only: :index
+  before_action :check_project, only: :index
 
   def index
   end
@@ -11,5 +13,13 @@ class TagsController < ApplicationController
   def correct_member
     @member = current_user.members.find_by(project_id: params[:project_id])
     redirect_to root_url if @member.nil?
+  end
+
+  def manage_tags
+    @tags = Tag.where("project_id = ?", params[:project_id]).page params[:page]
+  end
+
+  def check_project
+    @project = Project.find(params[:project_id])
   end
 end
--- src/spec/systems/tags/index_spec.rb
+++ src/spec/systems/tags/index_spec.rb
@@ -1,24 +1,66 @@
 require "rails_helper"
 
 RSpec.describe "Tag Index Page In The Project", type: :system do
-  describe "The process of accessing member index page in project" do
+  let(:owner){ FactoryBot.create(:owner_of_project_with_tags) }
+  let(:member){ FactoryBot.create(:member_of_project_with_tags) }
+  let(:the_other_user){ FactoryBot.create(:member) }
+
+  describe "The process of accessing tag index page in project" do
     context "when user didn't logged in" do
       it "shouldn't show tag index page" do
+        project = owner.project
+        visit project_tags_path(project)
+
+        expect(page).to have_text("Log in")
+        expect(page).to have_text("Sign up now !!")
       end
     end
     context "when user logged in" do
       context "as not a member of this project" do
+        before do
+          log_in_as(the_other_user.user)
+        end
         it "shouldn't show tag index page" do
+          project = owner.project
+          visit project_tags_path(project)
+
+          expect(page).to have_text("All Projects")
         end
       end
       # Member doesn't have the right to edit or destroy tags
       context "as a member of this project" do
+        before do
+          log_in_as(member.user)
+        end
         it "should show tag index page" do
+          project = member.project
+          tags = project.tags
+          visit project_tags_path(project)
+
+          expect(page).to have_link 'Add Tag'
+          tags.each do |tag|
+            expect(page).to have_text(tag.name)
+            expect(page).not_to have_link("edit_#{tag.id}")
+            expect(page).not_to have_link("remove_#{tag.id}")
+          end
         end
       end
-      # Member has the right to edit or destroy tags
+      # Owner has the right to edit or destroy tags
       context "as a owner of this project" do
+        before do
+          log_in_as(owner.user)
+        end
         it "should show tag index page" do
+          project = owner.project
+          tags = project.tags
+          visit project_tags_path(project)
+
+          expect(page).to have_link 'Add Tag'
+          tags.each do |tag|
+            expect(page).to have_text(tag.name)
+            expect(page).to have_link("edit_#{tag.id}")
+            expect(page).to have_link("remove_#{tag.id}")
+          end
         end
       end
     end
