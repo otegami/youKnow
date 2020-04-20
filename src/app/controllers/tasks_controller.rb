bb326bb9c67a5724f96820f4f5669587a6cde802
otegami <a.s.takuya1026@gmail.com>
2020-04-20 10:24:19 +0900

    [Progressing] create request spec and new action about task controller

--- src/app/controllers/tasks_controller.rb
+++ src/app/controllers/tasks_controller.rb
@@ -1,2 +1,13 @@
 class TasksController < ApplicationController
+  before_action :logged_in_user
+  before_action :correct_member, only: :new
+
+  def new
+  end
+
+  private
+  def correct_member
+    @member = current_user.member?(params[:project_id])
+    redirect_to root_url if @member.nil?
+  end
 end
---
+++ src/spec/requests/tasks/new_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe 'Tasks', type: :request do
+  describe '/projects/:project_id/tasks/new' do
+    let(:member){ FactoryBot.create(:member) }
+    let(:owner){ FactoryBot.create(:owner) }
+    let(:the_other_member){ FactoryBot.create(:member) }
+
+    context "when user didn't log in" do
+      it "shouldn't show new task's page" do
+        project = owner.project
+        get new_project_task_path(project)
+        expect(response).not_to have_http_status(:success)
+      end
+    end
+    context "when user logged in" do
+      context "as not a member" do
+        before do
+          log_in_as(the_other_member.user)
+        end
+        it "shouldn't show new task's page" do
+          project = owner.project
+          get new_project_task_path(project)
+          expect(response).not_to have_http_status(:success)
+        end
+      end
+      context "as a member" do
+        before do
+          log_in_as(member.user)
+        end
+        it "should show new task's page" do
+          project = member.project
+          get new_project_task_path(project)
+          expect(response).to have_http_status(:success)
+        end
+      end
+      context "as a owner" do
+        before do
+          log_in_as(owner.user)
+        end
+        it "should show new task's page" do
+          project = owner.project
+          get new_project_task_path(project)
+          expect(response).to have_http_status(:success)
+        end
+      end
+    end
+  end
+end
