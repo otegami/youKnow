054195f3d307eb18f6b746c24d35a65a29495491
otegami <a.s.takuya1026@gmail.com>
2020-04-21 15:13:36 +0900

    [Progressing] create model spec about pic

--- src/app/models/pic.rb
+++ src/app/models/pic.rb
@@ -1,4 +1,5 @@
 class Pic < ApplicationRecord
   belongs_to :user
   belongs_to :task
+  validates :owner, inclusion: { in: [true, false] }
 end
--- src/spec/models/pic_spec.rb
+++ src/spec/models/pic_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Pic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:pic){ FactoryBot.create(:pic) }
+  
+  describe "Validation" do
+    it "should check the existence of pic" do
+      expect(pic).to be_valid
+    end
+    it "should check user_id" do
+      pic.task_id = nil
+      expect(pic).to be_invalid
+    end
+    it "should check project_id" do
+      pic.user_id = nil
+      expect(pic).to be_invalid
+    end
+    it "should check owner" do
+      pic.owner = nil
+      expect(pic).to be_invalid
+    end
+  end
 end
